912f35c5c0d1f1a6abba7ddfd350727848ad32d9
joaorafaelalmeida <joao.rafael.almeida@ua.pt>
2018-09-13 16:22:40 +0100

    Development of the GUI for the protocol creation. Layout of all elements created. Missing the addition of the created elements to the protocol.

--- UI/src/js/components/protocolElements/ActionElement.js
+++ UI/src/js/components/protocolElements/ActionElement.js
@@ -1,21 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Reflux from 'reflux';
 import DisplayField from '../reusable/DisplayField.js';
 
 class ActionElement extends Reflux.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            nextElementId: (this.props.elementID + 1).toString(),
+            action: ""
+        };
     }
 
+    actionHandleChange  = (event) => {
+        event.preventDefault();
+        this.setState({action: event.target.value});
+    };
+
+    nextElementIdHandleChange = (event) => {
+        event.preventDefault();
+        this.setState({nextElementId: event.target.value});
+    };
 
     render() {
         return (
-            <p>
-                todo
-            </p>
+            <div>
+                <DisplayField label={"Action"}
+                              onChange={this.actionHandleChange}
+                              value={this.state.action}/>
+                <DisplayField label={"Next element"}
+                              onChange={this.nextElementIdHandleChange}
+                              value={this.state.nextElementId}
+                              type={"number"}
+                              min={"0"}/>
+            </div>
         );
     }
+
+    static propTypes = {
+        /**
+         * Next element id
+         * */
+        nextElementId: PropTypes.number,
+    };
 }
+
+ActionElement.defaultProps = {
+    elementID: 1
+};
 
 export default ActionElement;
--- UI/src/js/components/protocolElements/DecisionElement.js
+++ UI/src/js/components/protocolElements/DecisionElement.js
@@ -1,21 +1,94 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Reflux from 'reflux';
+import {ClinicalVariablesStore, ClinicalVariablesActions} from '../../reflux/ClinicalVariablesReflux.js';
+import DisplayOptionsField from '../reusable/DisplayOptionsField.js';
 import DisplayField from '../reusable/DisplayField.js';
 
 class DecisionElement extends Reflux.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.store = ClinicalVariablesStore;
+        this.state = {
+            nextElementIdTrue: (this.props.elementID + 1).toString(),
+            nextElementIdFalse: (this.props.elementID + 2).toString(),
+            cv: undefined,
+            condition: undefined,
+            value: ""
+        };
     }
 
-    
+    componentDidMount() {
+        ClinicalVariablesActions.loadCVHeaders();
+    }
+
+    cvSelectHandleChange = (selection) => {
+        this.setState({cv: selection.value});
+    };
+
+    conditionHandleChange = (selection) => {
+        this.setState({condition: selection.value});
+    };
+
+    nextElementIdWhenTrueHandleChange = (event) => {
+        event.preventDefault();
+        this.setState({nextElementId: event.target.value});
+    };
+
+    nextElementIdWhenFalseHandleChange = (event) => {
+        event.preventDefault();
+        this.setState({nextElementId: event.target.value});
+    };
+
+    valueInConditionHandleChange = (event) => {
+        event.preventDefault();
+        this.setState({value: event.target.value});
+    };
+
     render() {
+        const conditionOptions = [
+            //{value: 'switch', label: 'switch'}, todo
+            {value: '<', label: '<'},
+            {value: '>', label: '>'},
+            {value: '=', label: '=='}
+        ];
+
         return (
-            <p>
-                todo
-            </p>
+            <div>
+                <DisplayOptionsField label={"Clinical variable"}
+                                     options={this.state.headers}
+                                     onChange={this.cvSelectHandleChange}
+                                     className={"Selectx2"}/>
+                <DisplayOptionsField label={"Condition"}
+                                     options={conditionOptions}
+                                     onChange={this.conditionHandleChange}/>
+                <DisplayField label={"Value"}
+                              onChange={this.valueInConditionHandleChange}
+                              value={this.state.value}/>
+                <DisplayField label={"Next element - True"}
+                              onChange={this.nextElementIdWhenTrueHandleChange}
+                              value={this.state.nextElementIdTrue}
+                              type={"number"}
+                              min={"0"}/>
+                <DisplayField label={"Next element - False"}
+                              onChange={this.nextElementIdWhenFalseHandleChange}
+                              value={this.state.nextElementIdFalse}
+                              type={"number"}
+                              min={"0"}/>
+            </div>
         );
     }
+
+    static propTypes = {
+        /**
+         * Next element id
+         * */
+        nextElementId: PropTypes.number,
+    };
 }
+
+DecisionElement.defaultProps = {
+    elementID: 1
+};
 
 export default DecisionElement;
