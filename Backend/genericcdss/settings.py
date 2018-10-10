5033b7ee46101cafc408141f2a68f7b0e9963e8f
joaorafaelalmeida <joao.rafael.almeida@ua.pt>
2018-10-10 18:53:15 +0100

    Some changes in django settings

--- Backend/genericcdss/settings.py
+++ Backend/genericcdss/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+BASE_URL = os.environ.get('BASE_DIR', '') + '/'
+
+STATIC_URL = BASE_URL + 'static2/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -179,7 +182,7 @@ SESSION_COOKIE_NAME = os.environ.get('SESSION_COOKIE_NAME', 'genericcdss')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static2/'
+#STATIC_URL = '/static2/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
